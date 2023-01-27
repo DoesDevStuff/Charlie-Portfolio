4b38b2e100f8187c6a51a79b4fc8ffd4e4b2623a
CharlieTheIndieDev <74312830+DoesDevStuff@users.noreply.github.com>
2023-01-27 06:10:48 +0000

    multiple zoom in for images
    multi image

--- js/zoomin-img_modal.js
+++ js/zoomin-img_modal.js
@@ -1,4 +1,5 @@
-// Get the modal
+/*Single Image zoom i.e without using class
+    // Get the modal
 var modal = document.getElementById('zoomin-Modal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
@@ -12,13 +13,37 @@ img.onclick = function(){
     captionText.innerHTML = this.alt;
 }
 
+*/
+
+
+// create references to the modal...
+var modal = document.getElementById('zoomin-Modal');
+// to all images -- note I'm using a class!
+var images = document.getElementsByClassName('zoomin-Images');
+// the image in the modal
+var modalImg = document.getElementById("img01");
+// and the caption in the modal
+var captionText = document.getElementById("caption");
+
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  // and attach our click listener for this image.
+  img.onclick = function() {
+    
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+}
+
 
 // When the user clicks on <span> (x), close the modal
 modal.onclick = function() {
     img01.className += " out";
     setTimeout(function() {
        modal.style.display = "none";
-       img01.className = "modal-content";
+       img01.className = "zoomin-modal-content";
      }, 400);
     
  }
